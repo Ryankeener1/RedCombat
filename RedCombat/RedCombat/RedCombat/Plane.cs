a3f254289e7e28d50c99e9be610dcd70d69ae6f1
joelaugustine099 <38436652+joelaugustine099@users.noreply.github.com>
2018-04-25 11:37:58 -0500

    Add files via upload

--- RedCombat/RedCombat/RedCombat/Plane.cs
+++ RedCombat/RedCombat/RedCombat/Plane.cs
@@ -17,20 +17,50 @@ namespace RedCombat
         Rectangle Rect;
 
         Vector2 Velocity;
-        int ReloadTime;
+        int ReloadTime = 0;
+        int MAXReloadTime;
         int NumOfBulltes;
         bool Reloading;
 
+        Texture2D bulletTex;
 
-        public Plane(Texture2D t, Rectangle r, Vector2 v, int reloadT, int BulletNum)
+        public Plane(Texture2D t, Rectangle r, Vector2 v, int reloadT, int BulletNum, Texture2D bulletT)
         {
             Text2D = t;
             Rect = r;
             Velocity = v;
-            ReloadTime = reloadT;
+            MAXReloadTime = reloadT;
             NumOfBulltes = BulletNum;
+            bulletTex = bulletT;
         }
 
+        public void Update()
+        {
+            if (Reloading)
+            {
+                ReloadTime++;
+                if(ReloadTime >= MAXReloadTime)
+                {
+                    Reloading = false;
+                    ReloadTime = 0;
+                }
+            }
+        }
+
+
+        public void Shoot()
+        {
+            if (!Reloading)
+            {
+                Bullet b = new Bullet(bulletTex, new Rectangle(200, 200, 20, 10), Color.Blue, Math.PI, 3);
+                Reloading = true;
+            }
+        }
+
+        public void Respawn()
+        {
+
+        }
 
 
     }
